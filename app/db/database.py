88d9475cd2efeebe2b5778c49aaa731a5655c6bd
Zei1007 <zbersamolde03@gmail.com>
2025-09-23 07:18:28 +0800

    dbconfig

---
+++ app/db/database.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from .dbconf import DATABASE_URL
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        print("Attempting to connect to the database...")
+        print("Database connection established.")
+        yield db
+    except Exception as e:
+        print(f"Error connecting to the database: {e}")
+        raise # Re-raise the exception to propagate the error
+    finally:
+        print("Database closing.")
+        db.close()
+
+
--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
 from typing import Union
 from fastapi import FastAPI
+from .db.database import get_db
+from fastapi import Depends
+from .db.database import SessionLocal
+import os
+
 
 app = FastAPI()
 @app.get("/")
-def read_root():
-    return {"Helllo": "World"}
+def read_root(db: SessionLocal = Depends(get_db)):
+    # Your database operations here
+    return {"message": "Hello, database connected!"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
     return {"item_id": item_id}
+
+db_url = os.getenv('DATABASE_URL')
+print(f"Database URL: {db_url}")
